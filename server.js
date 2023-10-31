b7fb546c413870dc6e560c9480593faee71e548f
Kendall Pfenning <kendallpfenning@Kendalls-MacBook-Pro.local>
2023-10-30 19:23:25 -0700

    added seeds.js file

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const sequelize = require('./login/config/connection');
 const helpers = require('./login/utils');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 
 const sess = {
     secret: 'Super secret secret',
