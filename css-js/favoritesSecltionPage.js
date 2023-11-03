cb69825f361785d9c88f73bde448aa218d226f73
Trevis Williams <rst1426@gmail.com>
2023-11-02 21:26:06 -0700

    added resposive desgine

--- css-js/favoritesSecltionPage.js
+++ css-js/favoritesSecltionPage.js
@@ -1,4 +1,3 @@
-
 function toggleBackground(element) {
     const isActive = element.parentElement.classList.toggle('active');
     if (isActive) {
@@ -6,17 +5,24 @@ function toggleBackground(element) {
         const imageId = element.id; // Get the "id" of the clicked image
         // Save the "id" to local storage
         localStorage.setItem("selectedImageId", imageId);
-        // Add a delay before navigating to the next page
-        setTimeout(function () {
-            // Simulate a click on the "Next" button after a delay
-            const nextButton = document.getElementById("nextButton");
-            nextButton.click();
-        }, 2000); // Delay for 2 seconds (2000 milliseconds)
     } else {
         showNextButton(false);
         localStorage.removeItem("selectedImageId");
     }
 }
+
+function delayedNavigation() {
+    // Add a delay before navigating to the next page
+    setTimeout(function () {
+        // Simulate a click on the "Next" button after a delay
+        const nextButton = document.getElementById("nextButton");
+        nextButton.click();
+    }, 2000); // Delay for 2 seconds (2000 milliseconds)
+}
+
+// Assuming you have a button with an id "nextButton" that triggers navigation
+document.getElementById("nextButton").addEventListener("click", delayedNavigation);
+
 
 
 
