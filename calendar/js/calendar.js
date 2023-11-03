7ad877cf291b5ba9382aa3d22330bdad81400dbe
Kendall Pfenning <kendallpfenning@Kendalls-MacBook-Pro.local>
2023-11-02 20:05:12 -0700

    added local storage component to calendar.js

--- calendar/js/calendar.js
+++ calendar/js/calendar.js
@@ -1,4 +1,5 @@
 
+
 fetch('http://api.sportradar.us/nfl/official/trial/v7/en/games/current_season/schedule.json?api_key=qzzcby3fq9nxwz25zkfx6dj6')
     .then(response => {
         if(!response.ok) {
@@ -7,19 +8,35 @@ fetch('http://api.sportradar.us/nfl/official/trial/v7/en/games/current_season/sc
         return response.json();
     })
     .then(data => {
-        const games = data.weeks[0].games; 
-        games.forEach(game => {
-          const homeTeamName = game.home.name;
-          const awayTeamName = game.away.name;
-          const scheduledDate = game.scheduled;
-          const stadiumName = game.venue.name;
+        // grabs favorite team from local storage
+        const favoriteTeam = localStorage.getItem('selectedImageId');
     
-          
-          console.log(`Home Team: ${homeTeamName}`);
-          console.log(`Away Team: ${awayTeamName}`);
-          console.log(`Scheduled Date: ${scheduledDate}`);
-          console.log(`Stadium Name: ${stadiumName}`);
-        });
+        if (favoriteTeam) {
+          const gamesData = data.weeks; 
+    
+         // goes through each week of gamesData
+          for (const week of gamesData) {
+            // checks if week property has games property
+            if (week && week.games && Array.isArray(week.games)) {
+                // goes through each game
+              for (const game of week.games) {
+                const homeTeamName = game.home.name;
+                const awayTeamName = game.away.name;
+                const scheduledDate = game.scheduled;
+                const stadiumName = game.venue.name;
+                // checks if the game includes favoriteTeam
+                if (homeTeamName === favoriteTeam || awayTeamName === favoriteTeam) {
+                  console.log(`Home Team: ${homeTeamName}`);
+                  console.log(`Away Team: ${awayTeamName}`);
+                  console.log(`Scheduled Date: ${scheduledDate}`);
+                  console.log(`Stadium Name: ${stadiumName}`);
+                }
+              }
+            }
+          }
+        } else {
+          console.log('Favorite team not found in local storage.');
+        }
       })
       .catch(error => {
         console.error('Error:', error);
