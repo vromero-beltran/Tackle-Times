8795f999f1538b0b45d95380478cc12078d029ca
Trevis Williams <rst1426@gmail.com>
2023-11-05 20:45:43 -0800

    made it so logos show on favotie page

--- css-js/favoritesSecltionPage.js
+++ css-js/favoritesSecltionPage.js
@@ -1,15 +1,52 @@
 function toggleBackground(element) {
     const isActive = element.parentElement.classList.toggle('active');
+    const maxTeams = 6; // Maximum number of teams allowed
+
     if (isActive) {
-        showNextButton(true);
-        const imageId = element.id; // Get the "id" of the clicked image
-        // Save the "id" to local storage
-        localStorage.setItem("selectedImageId", imageId);
+        if (selectedTeams.length < maxTeams) {
+            showNextButton(true);
+            addTeamToSelection(element);
+        } else {
+            element.parentElement.classList.remove('active'); // Deselect the team
+            showNextButton(true); // Still show the "Next" button
+        }
     } else {
-        showNextButton(false);
-        localStorage.removeItem("selectedImageId");
+        removeTeamFromSelection(element);
     }
 }
+
+function addTeamToSelection(element) {
+    const teamName = element.alt;
+    const teamSrc = element.src;
+
+    // Assign teamId based on the position in the selectedTeams array (team 1 through team 6)
+    const teamId = `team ${selectedTeams.length + 1}`;
+
+    selectedTeams.push({ id: teamId, name: teamName, src: teamSrc });
+    updateLocalStorage();
+}
+
+function removeTeamFromSelection(element) {
+    const teamId = element.id;
+    const index = selectedTeams.findIndex(team => team.id === teamId);
+
+    if (index !== -1) {
+        selectedTeams.splice(index, 1);
+        updateLocalStorage();
+    }
+}
+
+function updateLocalStorage() {
+    localStorage.setItem("selectedTeams", JSON.stringify(selectedTeams));
+}
+
+
+
+
+
+
+
+
 
 function delayedNavigation() {
     // Add a delay before navigating to the next page
@@ -43,3 +80,23 @@ teamImages.forEach((image) => {
         toggleBackground(this);
     };
 });
+
+
+function handleTeamClick(teamId) {
+    // Store the selected team in local storage
+    localStorage.setItem('selectedTeam', teamId);
+
+    // Pass the image source URL as well
+    const teamImageSrc = document.getElementById(teamId).src;
+    localStorage.setItem('selectedTeamImage', teamImageSrc);
+
+    // Redirect to the second page
+    window.location.href = 'myfavoriteteams.html'; // Replace 'secondPage.html' with your actual second page's URL
+}
+
+
+const selectedTeams = [];
+
+
+
+// save teams to array with the labes team 1 team 2 team 3 team 4 team 5 team 6 then change id of them on the next page to team 1-6
--- css-js/myFavotiteTeams.js
+++ css-js/myFavotiteTeams.js
@@ -1,8 +1,29 @@
 
 
-function toggleBackground(element) {
-    element.classList.toggle("active");
+
+
+
+function initializeSelectedTeams() {
+    const selectedTeams = JSON.parse(localStorage.getItem("selectedTeams")) || [];
+
+    selectedTeams.forEach(team => {
+        // Find the corresponding team element by ID
+        const teamElement = document.getElementById(team.id);
+
+        if (teamElement) {
+            // Set the team's image source and ID
+            teamElement.src = team.src;
+            teamElement.id = team.id;
+
+            // Add the "active" class to mark it as selected
+            teamElement.parentElement.classList.add('active');
+        }
+    });
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    // Initialize the selected teams when the page loads
+    initializeSelectedTeams();
+});
 
 
