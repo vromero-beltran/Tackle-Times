dd0f6281757291902e1847e5937f8ff59fa0486e
Victor <frozen711.vr.vr@gmail.com>
2023-11-07 21:26:03 -0500

    updated login requirments

--- login/controllers/home-routes.js
+++ login/controllers/home-routes.js
@@ -5,89 +5,104 @@ const { Team, User } = require('../models');
 
 
 router.get('/', async (req, res) => {
-  try {
-    // Get all teams and JOIN with user data
-    const teamData = await Team.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
+  if (!req.session.loggedIn) {
+    res.redirect('/login');
+  } else {
+    // If the user is logged in, allow them to view the their teams
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Team }],
+      });
 
-    // Serialize data so the template can read it
-    const teams = teamData.map((team) => team.get({ plain: true }));
+      const user = userData.get({ plain: true });
 
-    // Pass serialized data and session flag into template
-    res.render('login', { 
-      teams, 
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+      res.render('favoritesSecltionPage', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+}
 });
 
 router.get('/team/:id', async (req, res) => {
-  try {
-    const teamData = await Team.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    const team = teamData.get({ plain: true });
-
-    res.render('team', {
-      ...team,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  // If the user is not logged in, redirect the user to the login page
+  if (!req.session.loggedIn) {
+    res.redirect('/login');
+    } else {
+      // If the user is logged in, allow them to view the teams
+      try {
+        const teamData = await Team.findByPk(req.params.id, {
+          include: [
+            {
+              model: User,
+              attributes: ['name'],
+            },
+          ],
+        });
+        const team = teamData.get({ plain: true });
+        res.render('team', {
+          ...team,
+          logged_in: req.session.logged_in
+        });
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    }
 });
 
 // Use withAuth middleware to prevent access to route
 router.get('/myfavoriteteams', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Team }],
-    });
+    // If the user is not logged in, redirect the user to the login page
+    if (!req.session.loggedIn) {
+      res.redirect('/login');
+    } else {
+      // If the user is logged in, allow them to view the their teams
+      try {
+        // Find the logged in user based on the session ID
+        const userData = await User.findByPk(req.session.user_id, {
+          attributes: { exclude: ['password'] },
+          include: [{ model: Team }],
+        });
 
-    const user = userData.get({ plain: true });
+        const user = userData.get({ plain: true });
 
-    res.render('myfavoriteteams', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+        res.render('myfavoriteteams', {
+          ...user,
+          logged_in: true
+        });
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    }
 });
 
 // Use withAuth middleware to prevent access to route
 router.get('/favoritesSecltionPage', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Team }],
-    });
+      // If the user is not logged in, redirect the user to the login page
+      if (!req.session.loggedIn) {
+        res.redirect('/login');
+      } else {
+        // If the user is logged in, allow them to view the their teams
+        try {
+          // Find the logged in user based on the session ID
+          const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Team }],
+          });
 
-    const user = userData.get({ plain: true });
+          const user = userData.get({ plain: true });
 
-    res.render('favoritesSecltionPage', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
+          res.render('favoritesSecltionPage', {
+            ...user,
+            logged_in: true
+          });
+        } catch (err) {
+          res.status(500).json(err);
+        }
   }
 });
 
